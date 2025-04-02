e9ac686d80f793f9cccf678d253368c522d1c92e
Shehab Tarek <back1ply@gmail.com>
2025-04-03 00:43:39 +0200

    more fix

--- 03_suggest_bronze_to_silver_transformations.Notebook/notebook-content.py
+++ 03_suggest_bronze_to_silver_transformations.Notebook/notebook-content.py
@@ -79,7 +79,7 @@ def suggest_transformations(df, table_name, column):
 
 # List of bronze tables and columns to inspect
 targets = [
-    ("bronze.crm_cust_info", ["cst_id", "cst_key", "cst_firstname", "cst_lastname", "cst_marital_status", "cst_gndr"]),
+    ("bronze.crm_cust_info", ["cst_id", "cst_firstname", "cst_lastname", "cst_marital_status", "cst_gndr", "cst_create_date"]),
     ("bronze.crm_prd_info", ["prd_id", "prd_key", "prd_nm", "prd_cost", "prd_line"]),
     ("bronze.crm_sales_details", ["sls_ord_num", "sls_prd_key", "sls_cust_id", "sls_sales", "sls_quantity", "sls_price"]),
     ("bronze.erp_cust_az12", ["cid", "gen"]),
